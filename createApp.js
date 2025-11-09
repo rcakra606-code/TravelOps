7f3df8f386cbbd5793581e5d43cf00fa2ccd0180
rcakra606-code <rcakra606@gmail.com>
2025-11-09 14:19:12 +0700

    Fix CSP: Allow Google Fonts for stylesheets and fonts

--- createApp.js
+++ createApp.js
@@ -45,7 +45,8 @@ export async function createApp() {
       directives: {
         defaultSrc: ["'self'"],
         scriptSrc: ["'self'"],
-        styleSrc: ["'self'", "'unsafe-inline'"],
+        styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
+        fontSrc: ["'self'", "https://fonts.gstatic.com"],
         imgSrc: ["'self'", 'data:'],
         connectSrc: ["'self'"],
         objectSrc: ["'none'"],
